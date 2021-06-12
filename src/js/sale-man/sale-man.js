c1a77cbd477f58c5e28e1035b8defe86523b58a0
hunghanhphuc1428 <hunghanhphuc1428@gmail.com>
2021-06-12 21:47:40 +0700

    them saleman

---
+++ src/js/sale-man/sale-man.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function (){
+    return (
+        <div className="home_content">
+        <div className="text">
+          {/* ############################ */}
+
+
+
+
+          {/* ################################# */}
+        </div>
+      </div>
+    );
+}
